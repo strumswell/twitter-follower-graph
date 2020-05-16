eb12cfc0d8adc2bf79e650f07517d7e210e36ae9
strumswell <philipp.bukkit@gmail.com>
2020-05-16 09:22:53 +0200

    added script and updated names

---
+++ get_followers_2nd_level.py
@@ -0,0 +1,31 @@
+import csv
+from bluebird import BlueBird
+
+bb = BlueBird()
+
+def get_followers(username):
+    return bb.get_followers(username)
+
+def save_result(_filename, _followers):
+    with open('/home/philipp/tw-scraper/gruene/users/'+_filename+'.csv', 'w', newline='') as file:
+        for _user in _followers:
+            file.write(_user+"\n")
+
+def load_csv():
+    with open('/home/philipp/tw-scraper/gruene/gruene1.csv', newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        return data
+
+
+print("Now for its followers...")
+followers = load_csv() # containing all username that follow the head account
+i = 1
+for user in followers:
+    print("Scraping "+user[0]+" ("+str(i)+"/"+str(len(followers))+")")
+    try:
+        save_result(user[0], get_followers(user[0]))
+    except: 
+        save_result(user[0], [''])
+        print('Error with user...') # Most of the time the user deletet their account
+    i += 1
